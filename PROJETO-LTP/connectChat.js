fa5c48000f3298ac02330572e2826711e01b68ce
Betrizav <133694594+Betrizav@users.noreply.github.com>
2023-10-26 07:46:37 -0400

    Add files via upload

---
+++ PROJETO-LTP/connectChat.js
@@ -0,0 +1,25 @@
+async function conectApiChatGPT(pergunta) {
+    const apiKey = "sk-sTUJkg8nch8CcAujMeAJT3BlbkFJnuTqXmPbAXL0sPj8VQy";
+    try {
+      const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          Accept: "Application/json",
+          "Content-Type": "Application/json",
+          Authorization: `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          "messages": [{ "role": "user", "content": pergunta }],
+          "temperature": 0.7
+        }),
+      });
+      const responseFormat = await response.json();
+      console.log(responseFormat);
+      const resposta = await responseFormat.choices[0].message.content;
+      return resposta;
+ 
+    } catch (err) {
+      return "Ocorreu um erro na operação";
+    }
+  }
