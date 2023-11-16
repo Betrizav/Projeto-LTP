43187b5b9c593e6b4cd8cd4e2e24caf62b7106d6
Beatrizavn <beatriz.vieira@estudante.ifms.edu.br>
2023-11-16 07:55:48 -0400

    m

--- PROJETO-LTP/index.js
+++ PROJETO-LTP/index.js
@@ -1,10 +1,22 @@
+class Connect{
+  constructor(pergunta){
+    this.pergunta = pergunta;
+  }
+  enviarPergunta(){
+    const pergunta = connectApiChatGPT(this.pergunta);
+    return pergunta;
+  }
+}
+
 const enviarPergunta = async() => {
     const pergunta = document.querySelector("#pergunta")
 
     const spinner = document.querySelector("#spinner")
      spinner.style.display = "block";
 
-    const resposta = await connectApiChatGPT(pergunta.value);
+    const connect = new Connect(pergunta.value);
+
+    const resposta = await connect.enviarPergunta();
 
     spinner.style.display = "none";
 
